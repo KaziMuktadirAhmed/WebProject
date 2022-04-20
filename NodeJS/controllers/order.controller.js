243981be33a6f8343d09911170d1a8b7e719de19
Kazi Muktadir Ahmed <bsse1111@iit.du.ac.bd>
2022-04-21 04:22:55 +0600

    order api back end

--- NodeJS/controllers/order.controller.js
+++ NodeJS/controllers/order.controller.js
@@ -1,6 +1,36 @@
 const express = require('express');
 var router = express.Router();
 
-var Order = require('../models/order');
+var { Order } = require('../models/order');
+
+router.get('/', (req, res) => {
+    var order = new Order({
+        customer: req.body.customer
+    });
+
+    Order.find({customer: order.customer}, (err, docs) => {
+        if(!err) {
+            if(docs)
+                res.send(docs);
+            else
+                return res.send(400).status('Could not find order for customer: ' + order.customer);
+        }   
+        else
+            console.log('Error in finding order: ' + JSON.stringify(err, undefined, 2));
+    });
+});
+
+router.post('/', (req, res) => {
+    var order = new Order({
+       itemname: req.body.itemname,
+       customer: req.body.customer,
+       quantity: req.body.quantity,
+    });
+
+    order.save((err, doc) => {
+        if(!err)    res.send(doc);
+        else        console.log('Error in recording order: ' + JSON.stringify(err, undefined, 2));
+    });
+});
 
 module.exports = router;
--- NodeJS/index.js
+++ NodeJS/index.js
@@ -4,6 +4,7 @@ const cors = require('cors');
 
 const { mongoose } = require('./db');
 var customerController = require('../NodeJS/controllers/customer.controller');
+var orderController = require('../NodeJS/controllers/order.controller')
 
 var app = express();
 app.use(bodyParser.json());
@@ -14,4 +15,5 @@ app.listen(3000, () => console.log('server started at port: 3000'));
 
 
 app.use('/customer', customerController);
+app.use('/order', orderController);
 
